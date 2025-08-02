2726686655a9ff313017a19f6ac4aec49a354cf9
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-02 13:08:48 +0530

    re-added controler

--- src/main/java/com/example/rmmagent/controller/HealthController.java
+++ src/main/java/com/example/rmmagent/controller/HealthController.java
@@ -1,14 +1,13 @@
+package com.example.rmmagent.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HealthController {
 
-    @GetMapping("/healthz")
-    public ResponseEntity<String> healthCheck() {
-        try {
-            // Simulate delay for 3 minutes (for readiness probe failure testing)
-            Thread.sleep(180000);
-        } catch (InterruptedException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Interrupted");
-        }
-        return ResponseEntity.ok("Application is healthy");
-    }
+	    @GetMapping("/health")
+	        public String healthCheck() {
+			        return "RMM Agent v3.1 is up and running!";
+				    }
 }
